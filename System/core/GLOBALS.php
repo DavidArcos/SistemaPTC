5dc3548c3107ff6c7491107fa99d6750e9f547cb
Angel Perez <angyay0@bitbucket.org>
2015-12-05 19:41:28 -0600

    Implementacion del ORM...y definicion de modelos

---
+++ System/core/GLOBALS.php
@@ -0,0 +1,14 @@
+<?php
+    define("ROUTE","");
+    define("REDBEAN","../core/rb.php");
+    define("HOSTNAME","localhost");
+    define("DBPREFIX","");
+    define("DBNAME","");
+    define("DBUSER","");
+    define("DBPASS","");
+    define("VIEW","../View/");
+    define("CONTROLLER","../Controller/");
+    define("MODEL","../Model/");
+    define("SITE_NAME","Systema PTC");
+    define("ERROR_PAGE","../core/404.html");
+ ?>
